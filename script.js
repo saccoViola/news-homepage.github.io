92f64f974dec45bbea6c895a74f0806abf75ee08
saccoViola <168297914+saccoViola@users.noreply.github.com>
2025-03-05 14:00:47 +0100

    fixing media-query
    Fixing media query and html labels

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-//x.addEventListener("click", (e) =>{
-// topMenu.style.display = "none"
-//})
-
-//immagineHamburger.addEventListener("click" (e) =>{
-// topMenu.style.display = "flex";
-// //})
-
-
-//quando topmenu è attivo, cambio immagine e metto la X e aggiungo classe z-index
-
 const menuIcon = document.querySelector(".hamburger-menu");
 const topMenu = document.querySelector(".top-menu");
 
